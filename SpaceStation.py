af404ed0c2b93094658b847e98edd8f31c8d2236
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-11-13 21:03:18 -0800

    Add files via upload

---
+++ SpaceStation.py
@@ -0,0 +1,33 @@
+# Name: First Last
+# Date:
+# Period:
+# Lab: SpaceStation.py
+# Description: Students use web services to gather and process live data from the internet.
+#
+#     Style - Code format, whitespace and PEP-8 style is followed making code easy to read.
+#     Comments - Blocks of code are well commented, every function has a descriptive comment.
+#     Tests -   The program runs as described in the specifications without errors(passes all tests).
+#
+
+import json  # to interpret JSON data from a web service
+import urllib.request  # to load data from a specific web service api
+import turtle  # for images
+
+# TODO: Step 1 Who is in space?
+
+
+# TODO: Step 2 Where is the ISS?
+
+
+# TODO: Step 3 When will the ISS be overhead?  Note: Change form Houston to Roseville
+
+
+# TODO: Step 4 Rewrite the program in a format that uses functions
+
+
+# TODO: Step 5 How could you make the program more interactive?
+# Click for new overhead location?
+# Ask User for location for overhead location?
+# Make the program update automatically?
+
+# use screen.mainloop() to keep the window open
